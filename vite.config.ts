2655eec9142624336892f37a049f3adaf627c397
xeptore <29199390+xeptore@users.noreply.github.com>
2026-08-14 11:03:39 +0330

    feat: update app icons and configuration
    - Added multiple new icon sizes (72x72, 96x96, 128x128, 144x144, 152x152, 192x192, 384x384, 512x512) in PNG format.
    - Updated the Vite configuration to reference the new icons and removed old icon files (192.png, 512.png, and icon.svg).
    - Changed the main icon file from SVG to PNG format for better compatibility.
    
    Signed-off-by: xeptore <29199390+xeptore@users.noreply.github.com>

--- vite.config.ts
+++ vite.config.ts
@@ -18,21 +18,57 @@ export default defineConfig({
         start_url: '/',
         icons: [
           {
-            src: '/icon-192.png',
-            sizes: '192x192',
+            src: '/icon-72x7x.png',
+            sizes: '72x72',
             type: 'image/png',
-            purpose: 'any'
+            purpose: 'any maskable'
           },
           {
-            src: '/icon-512.png',
+            src: '/icon-96x96.png',
+            sizes: '96x96',
+            type: 'image/png',
+            purpose: 'any maskable'
+          },
+          {
+            src: '/icon-128x128.png',
+            sizes: '128x128',
+            type: 'image/png',
+            purpose: 'any maskable'
+          },
+          {
+            src: '/icon-144x144.png',
+            sizes: '144x144',
+            type: 'image/png',
+            purpose: 'any maskable'
+          },
+          {
+            src: '/icon-152x152.png',
+            sizes: '152x152',
+            type: 'image/png',
+            purpose: 'any maskable'
+          },
+          {
+            src: '/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'any maskable'
+          },
+          {
+            src: '/icon-384x384.png',
+            sizes: '384x384',
+            type: 'image/png',
+            purpose: 'any maskable'
+          },
+          {
+            src: '/icon-512x512.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'any maskable'
           },
           {
-            src: '/icon.svg',
+            src: '/icon.png',
             sizes: 'any',
-            type: 'image/svg+xml',
+            type: 'image/png',
             purpose: 'any maskable'
           }
         ]
